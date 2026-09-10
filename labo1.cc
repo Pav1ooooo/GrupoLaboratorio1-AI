b50f88e476aff5e902c81ec8c4db7aa2c53db02b
Pav1ooooo <00030325@uca.edu.sv>
2026-09-10 10:44:23 -0600

    feat agregamos funcion ConsultarCombustible

--- labo1.cc
+++ labo1.cc
@@ -1,6 +1,17 @@
 #include <iostream>
-int main (){
-    hola
-  return 0;
-  
+
+// 1. Consultar combustible (Paso por valor)
+void ConsultarCombustible(float combustible) {
+    std::cout << "Combustible actual: " << combustible << " litros.\n";
+}
+
+int main() {
+
+  std::cout <<"Sistema de gestion de combustible \n";
+    float combustible = 50;
+
+    std::cout << "=== PRUEBA CONSULTAR COMBUSTIBLE ===\n";
+    ConsultarCombustible(combustible);
+
+    return 0;
 }
